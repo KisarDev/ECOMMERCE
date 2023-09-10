773941babd3d6fba97e74baffcd1296f707a9581
unity-team-developers <cesarmartins.pro@gmail.com>
2023-09-10 17:44:03 +0100

    WIP: modelando produtos

--- pedido/models.py
+++ pedido/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Pedido(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
+    status = models.CharField(
+        default="C",
+        max_length=1,
+        choices= (
+            ('A', 'Aprovado'),
+            ('C', 'Aprovado'),
+            ('R', 'Aprovado'),
+            ('P', 'Aprovado'),
+            ('E', 'Aprovado'),
+            ('F', 'Aprovado'),
+            ('A', 'Aprovado'),
+            ('A', 'Aprovado'),
+            ('A', 'Aprovado'),
+            ('A', 'Aprovado'),
+            ('A', 'Aprovado'),
+
+        )
+    )
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.CharField
