7738dfed1f1d87f41ba3b20cc2d3949df222bcf6
unity-team-developers <cesarmartins.pro@gmail.com>
2023-09-11 20:41:05 +0100

    Fix templates

--- produto/views.py
+++ produto/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
+from . import models
 
-class ListaProduto(ListView):
-    pass
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
 class DetalheProduto(View):
     pass
 class AdicionarAoCarrinho(View):
