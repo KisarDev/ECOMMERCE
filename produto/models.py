b56484e347b9c38e38b87fd86aff6fdd4068e798
unity-team-developers <cesarmartins.pro@gmail.com>
2023-09-13 13:23:25 +0100

    WIP: pagination, formata_preco

--- produto/models.py
+++ produto/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.utils.text import slugify
 from PIL import Image
 import os
+from utils.utils import formata_preco
 # Create your models here.
 
 class Produto(models.Model):
@@ -17,12 +18,12 @@ class Produto(models.Model):
                                                                          ('S', 'Simples' )))
     
     def get_preco_formatado(self):
-        return f'R${self.preco_marketing:.2f}'.replace('.', ',')
+        return formata_preco(self.preco_marketing)
     get_preco_formatado.short_description = 'Preço'
     
 
     def get_preco_promo_formatado(self):
-        return f'R${self.preco_marketing_promocional:.2f}'.replace('.', ',')
+        return formata_preco(self.preco_marketing_promocional)
     get_preco_promo_formatado.short_description = 'Preço Promo'
 
 
--- produto/templatetags/omfilters.py
+++ produto/templatetags/omfilters.py
@@ -1,7 +1,8 @@
 from django.template import Library
-
+from utils import utils
 register = Library()
 
 @register.filter
 def formata_preco(val):
-    return f'R$ {val:.2f}'.replace('.', ',')
+    return utils.formata_preco(val)
+
